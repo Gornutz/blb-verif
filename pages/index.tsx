7d57f8e96b55de5340bb5a97df74743e4672345f
Gornutz <90406700+Gornutz@users.noreply.github.com>
2022-09-21 23:03:30 -0500

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import type { NextPage } from "next";
+import Image from 'next/image'
 import styles from "../styles/home.module.scss";
 import Link from 'next/link'
 
